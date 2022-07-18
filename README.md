49775e56f044301e64b0f5141031de770de9f80f
ItsMyLife <34112129+itsmylife44@users.noreply.github.com>
2022-07-18 22:50:18 +0200

    Create script & remove redist.exe due to license

---
+++ README.md
@@ -0,0 +1,3 @@
+# Assetto Corsa Competizione Docker container
+Windows nanoserver based  on Docker container for Assetto Corsa Competizione Dedicated Server !
+
---
+++ excecute.ps1
@@ -0,0 +1,10 @@
+# Create Folder
+
+mkdir .\bin\
+
+# Jump into bin folder
+cd .\bin\
+
+# Download vc redist
+wget https://aka.ms/vs/16/release/vc_redist.x64.exe -OutFile vc_redist.x64.exe
+
